448e4abf9963d0b5ef5474a1965ce64544c59b20
Valter Gomes <valtergnn@gmail.com>
2018-06-05 16:32:30 -0300

    Initial commit

---
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -0,0 +1,58 @@
+package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    private ImageView botaoEmpresa;
+    private ImageView botaoServico;
+    private ImageView botaoCliente;
+    private ImageView botaoContato;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoEmpresa  = findViewById(R.id.empresaId);
+        botaoServico = findViewById(R.id.servicosId);
+        botaoCliente = findViewById(R.id.clientesId);
+        botaoContato = findViewById(R.id.contatosId);
+
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        botaoServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ServicosActivity.class));
+            }
+        });
+
+        botaoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ClienteActivity.class));
+            }
+        });
+
+        botaoContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ContatoActivity.class));
+            }
+        });
+
+    }
+}
